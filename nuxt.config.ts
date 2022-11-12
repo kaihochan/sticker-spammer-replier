441e6e8d0dae47c2af1afc5777620663e58f2f0f
Kai Ho CHAN <kaihojohnchan@gmail.com>
2022-11-12 21:43:59 +0800

    Config file updated.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,4 +26,10 @@ export default defineNuxtConfig({
       'process.env.DEBUG': false,
     },
   },
+
+  runtimeConfig: {
+    public: {
+      baseURL: '/sticker-spammer-replier/',
+    },
+  },
 })
