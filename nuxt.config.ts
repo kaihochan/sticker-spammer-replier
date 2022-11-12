137fb068249fe7e50d86c0ee57ea9f83a57cc26b
Kai Ho CHAN <kaihojohnchan@gmail.com>
2022-11-12 21:02:29 +0800

    Webpage source code

---
+++ nuxt.config.ts
@@ -0,0 +1,29 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  ssr: true,
+
+  css: ['vuetify/lib/styles/main.sass'],
+
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  app: {
+    head: {
+      title: 'Sticker Spammer Replier',
+      charset: 'utf-8',
+      meta: [
+        { charset: 'utf-8' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' },
+      ],
+    },
+  },
+
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  },
+})
